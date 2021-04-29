876d3341ef3f96a6569060412cfba7b6a5556d69
Nata <mail.to.nata.vas@gmail.com>
2021-04-29 09:45:22 +0300

    creation mvvm app

---
+++ MVVM_App/MVVM_App/Infrastucture/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_App
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> action;
+        private readonly Predicate<object> predicate;
+
+        public RelayCommand(Action<object> action, Predicate<object> predicate)
+        {
+            this.action = action;
+            this.predicate = predicate;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        public bool CanExecute(object parametr)
+        {
+            return predicate == null ? true : predicate(parametr);
+        }
+        public void Execute(object parametr)
+        {
+            action(parametr);
+        }
+    }
+}
---
+++ MVVM_App/MVVM_App/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MVVM_App
+{
+    class MainViewModel
+    {
+        //public ICommand AddCommand { get; set; }
+        //public ObservableCollection<Student> Students { get; set; }
+    }
+}
